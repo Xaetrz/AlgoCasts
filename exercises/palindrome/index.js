7b1c843ffd2d7e5d23f415e9f7e5f0c615a64594
jkress <jonathank1993@gmail.com>
2023-05-04 19:52:22 -0500

    Add fib, palindrome, and reverse string solutions

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    // for (let i = 0; i < str.length / 2; i++) {
+    //     if (str[i] !== str[str.length - i - 1]) return false;
+    // }
+    // return true;
+
+    // Completely absurd alternative solution. Reverse the second half of the string and compares it to the first half.
+    // floor/ceiling of the middle index necessary to handle palindromes with an odd number of characters. Note that the end index of the slice method is non-inclusive.
+    let strArr = str.split('');
+    let midIdx = strArr.length / 2;
+    let firstHalf = strArr.slice(0, Math.ceil(midIdx)).join('');
+    let revSecondHalf = strArr.slice(Math.floor(midIdx), strArr.length).reverse().join('')
+    return firstHalf === revSecondHalf;
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // lol - the lazy man's implementation via str->arr->str
+    //return str.split('').reverse().join('');  
+    
+    // Simple reverse loop
+    // A stringbuilder would be much better in other languages, but JS optimizes string concatenation
+    // let revStr = "";
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     revStr += str[i];
+    // }
+    // return revStr;
+
+    // Cleaner for syntax
+    // let revStr = "";
+    // for (let char of str) {
+    //     revStr = char + revStr;
+    // }
+    // return revStr;
+
+    // Fun with the reduce function
+    // return str.split('').reduce( (previousValue, currentValue) => { 
+    //     return currentValue + previousValue; 
+    // }, "" );
+
+    // Fun with the reduce function, newer syntax
+    return str.split('').reduce( (rev, char) => char + rev, "" );
+}
 
 module.exports = reverse;
