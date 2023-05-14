23e087481398e7ee915b12d23c05ad3b01879c4d
jkress <jonathank1993@gmail.com>
2023-05-14 10:53:27 -0500

    Add memoization generator for fib; cleanup

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,25 +9,31 @@
 //   fib(4) === 3
 
 function fib(n) {
-    // Iterative solution
-    // if (n <= 2) return 1;
-    // let curr = 1;
-    // let prev = 1;
-    // for (let i = 3; i <= n; i++) {
-    //     let temp = curr;
-    //     curr += prev;
-    //     prev = temp;
-    // }
-    // return curr;
-
-    // Inefficient recursive solution
-    // if (n <= 2) return 1;
-    // return fib(n - 1) + fib(n - 2);
-
-    // Recursive solution with memoization
-    return fibMemo(n);
+    //return fibMemo(n);
+    fibSlow = memoize(fibSlow);  // VERY IMPORTANT: Note there's no let. We're overwriting the fibSlow function with a memoized version of itself
+    return fibSlow(n);
 }
 
+// Iterative solution
+function fibIterative(n) {
+    if (n <= 2) return 1;
+    let curr = 1;
+    let prev = 1;
+    for (let i = 3; i <= n; i++) {
+        let temp = curr;
+        curr += prev;
+        prev = temp;
+    }
+    return curr;
+}
+
+// Inefficient recursive solution
+function fibSlow(n) {
+    if (n <= 2) return 1;
+    return fibSlow(n - 1) + fibSlow(n - 2);
+}
+
+// Faster implementation of fib using memoization
 function fibMemo(n, memo = {}) {
     if (n <= 2) {
         memo[n] = 1;
@@ -37,6 +43,17 @@ function fibMemo(n, memo = {}) {
     let fibNMinus2 = memo[n - 2] || fibMemo(n - 2, memo);
     memo[n] = fibNMinus1 + fibNMinus2;
     return memo[n];
+}
+
+// A generic memoization function. Can pass in fibSlow to implement memoization
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) return cache[args];
+        
+        cache[args] = fn.apply(this, args);  // Alternatively: fn(...args)
+        return cache[args];
+    };
 }
 
 module.exports = fib;
